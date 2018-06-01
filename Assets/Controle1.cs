8c26d1bed76ce124a637d1b6c87bbfe1fee66545
Diego Gomes de Lima <diegogl13@hotmail.com>
2018-06-01 11:37:43 -0300

    refazendo o projeto em unity

---
+++ Assets/Controle1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controle1 : MonoBehaviour {
+
+    public float velocidade;
+    public float força_pulo;
+
+    private Rigidbody2D rb;
+    private bool orientadoDireita = true;
+    private bool pulo = false;
+    private animator anim;
+    private bool noChao;
+    private Transform checando_chao;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<>
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
