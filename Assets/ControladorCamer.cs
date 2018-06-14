dca297b69000796fbcb8e138334d6f2e36358d72
Diego Gomes de Lima <diegogl13@hotmail.com>
2018-06-14 00:40:06 -0300

    controles

---
+++ Assets/ControladorCamer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCamer : MonoBehaviour {
+
+    public GameObject Megaman;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - Megaman.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate() {
+        transform.position = Megaman.transform.position + offset;
+    }
+}
---
+++ Assets/controlePlayer1.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlePlayer1 : MonoBehaviour {
+
+    public float speed;
+    public float jumpforce;
+
+    private Rigidbody2D rb;
+    private bool faceright = true;
+    private bool jump = false;
+    private Animator anim;
+    private bool noChao = false;
+    private Transform groundcheck;
+
+
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        groundcheck = gameObject.transform.Find("GroundCheck");
+	}
+	
+	void Update () {
+        noChao = Physics2D.Linecast(transform.position, groundcheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if(Input.GetButtonDown("Jump") && noChao)
+        {
+            jump = true;
+            anim.SetTrigger("Pulou");
+        }
+	}
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("Velocidade", Mathf.Abs(h));
+
+        rb.velocity = new Vector2(h * speed, rb.velocity.y);
+
+        if (h > 0 && !faceright)
+            Flip();
+        else if (h < 0 && faceright)
+            Flip();
+
+        if(jump)
+        {
+            
+            rb.AddForce(new Vector2(0, jumpforce));
+            
+            jump = false;
+        }
+    
+    }
+
+    void Flip()
+    {
+        faceright = !faceright;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
+
+    }
+}
